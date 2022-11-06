dd386559fdfb6948b64eed427d16d970cbda931f
Jason Liggayu <9956289+jasonligg@users.noreply.github.com>
2022-11-05 21:58:21 -0400

    prettier single quote; prettify; setup tailwind for app dir

---
+++ src/app/layout.tsx
@@ -0,0 +1,16 @@
+import './globals.css';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html>
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,3 @@
+export default function App() {
+  return <div className="h-screen text-red-400">App</div>;
+}
