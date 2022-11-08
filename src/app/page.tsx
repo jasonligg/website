f141793475bf7b406474393de7fb8e4df6d32e8f
Jason Liggayu <9956289+jasonligg@users.noreply.github.com>
2022-11-07 23:40:55 -0500

    add header and subheader texta

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,21 @@
 export default function App() {
   return (
-    <main className="flex flex-row justify-center ">
-      <div className="">hello world.</div>
+    <main className="md:px-18 px-4 sm:px-6 lg:px-44 xl:px-96">
+      {/* <div>hello world.</div> */}
+      <hgroup>
+        <h1 className="text-5xl font-bold tracking-tight lg:text-6xl">
+          software engineer
+          <br />
+          coding educator
+          <br />
+          tech enthusiast
+        </h1>
+        <p className="max-w-prose pt-4 text-lg tracking-wide">
+          <strong>Hey!</strong> I&apos;m Jason, a software engineer based in New
+          York City. I love building cool stuff with code and helping others
+          along with their own coding journeys.
+        </p>
+      </hgroup>
     </main>
   );
 }
