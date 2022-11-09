50b924daedecf3af81b59a540ba415cbb26ba491
Jason Liggayu <9956289+jasonligg@users.noreply.github.com>
2022-11-08 23:18:47 -0500

    fix text font size for mobile view

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,19 +14,19 @@ export default function App() {
             width={100}
             className="my-6 rounded-full"
           />
-          <h1 className="whitespace-nowrap text-5xl font-bold tracking-tight 2xl:text-6xl">
+          <h1 className="whitespace-nowrap text-4xl lg:text-5xl font-bold tracking-tight 2xl:text-6xl">
             software engineer
             <br />
             coding educator
             <br />
             <HomepageHeroText />
           </h1>
-          <p className="max-w-prose pt-4 text-lg tracking-wide">
+          <p className="max-w-prose pt-4 lg:text-lg tracking-wide">
             <strong>Hey!</strong> I&apos;m Jason, a software engineer based in
             New York City. I love building cool stuff with code and helping
             others along with their own coding journeys.
           </p>
-          <p className="max-w-prose pt-4 text-lg tracking-wide">
+          <p className="max-w-prose pt-4 lg:text-lg tracking-wide">
             <strong>Currently,</strong> I&apos;m helping build the 100%
             renewable future and fighting the climate crisis at{" "}
             <a href="https://www.arcadia.com/" className="text-[#0EFF85]">
@@ -38,7 +38,7 @@ export default function App() {
         {/* <Image
           src={cat}
           alt="Picture of a grey cat wearing sunglasses."
-          className="md:pl-none min-w-fit object-contain pl-16"
+          className="md:pl-16 min-w-fit object-contain"
         /> */}
       </section>
     </main>
